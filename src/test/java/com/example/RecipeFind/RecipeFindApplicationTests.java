c600c90ba6d65f368cea6bd74c35ef2e70ac3861
Mihașca Rareș <mrrmtrd@gmail.com>
2024-02-20 16:37:28 +0200

    1

---
+++ src/test/java/com/example/RecipeFind/RecipeFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecipeFind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
